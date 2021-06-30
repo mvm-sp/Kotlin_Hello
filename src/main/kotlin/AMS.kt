9a640c75a9814426357cfb19158ccd34d5075922
Marcondes <mvm.sp11@gmail.com>
2021-06-30 12:12:56 -0300

    Initial commit

---
+++ src/main/kotlin/AMS.kt
@@ -0,0 +1,85 @@
+import java.util.*
+/*
+fun main (args: Array<String>) {
+    dayOfWeek()
+}
+fun main (args: Array<String>) {
+    val period = {if (args[0].toInt() < 12)  "Morning" else "Afternoon" }
+    println("Good $period")
+}
+
+fun main(args: Array<String>) {
+    println("\nYour fortune is: ${getFortune(getBirthday())}")
+}
+
+fun main(args: Array<String>) {
+    println("\nYour fortune is: ${getFortune(getBirthday())}")
+}
+
+*/
+
+
+
+fun main(args: Array<String>) {
+    println(whatShouldIDoToday("happy", "sunny"))
+    println(whatShouldIDoToday("sad"))
+    print("How do you feel?")
+    println(whatShouldIDoToday(readLine()!!))
+}
+
+fun isVeryHot (temperature: Int) = temperature > 35
+fun isSadRainyCold (mood: String, weather: String, temperature: Int) =
+    mood == "sad" && weather == "rainy" && temperature == 0
+fun isHappySunny (mood: String, weather: String) = mood == "happy" && weather == "sunny"
+fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24) : String {
+    return when {
+        isVeryHot(temperature) -> "go swimming"
+        isSadRainyCold(mood, weather, temperature) -> "stay in bed"
+        isHappySunny(mood, weather) -> "go for a walk"
+        else -> "Stay home and read."
+    }
+}
+
+fun canAddFish(tankSize : Float,
+               currentFish : List<Int>,
+               fishSize: Int,
+               hasDecorations : Boolean = false) : Boolean{
+
+    return (tankSize * if (hasDecorations) 0.8 else 1.0) >= (currentFish.sum() + fishSize)
+}
+
+fun getBirthday(): Int {
+    print("\nEnter your birthday: ")
+    return readLine()?.toIntOrNull() ?: 1
+}
+
+fun getFortune(birthday: Int): String {
+    val fortunes = listOf("You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends, because they are your greatest fortune.")
+    val index = when (birthday) {
+        in 1..7 -> 4
+        28, 31 -> 2
+        else -> birthday.rem(fortunes.size)
+    }
+    return fortunes[index]
+}
+
+fun dayOfWeek() {
+    var day : String? = null
+    day = when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
+        1 -> "Sunday"
+        2 -> "Monday"
+        3 -> "Tuesday"
+        4 -> "Wednesday"
+        5 -> "Thursday"
+        6 -> "Friday"
+        7 -> "Saturday"
+        else -> "Time has stopped"
+    }
+    println("What day is it today?\n$day")
+}
